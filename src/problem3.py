68d173c0f69d35cb25befb564fd5142fe452add0
saylorsa <saylorsa@rose-hulman.edu>
2019-03-19 21:42:27 -0400

    Exam 1, problem3, todo 2 Done

--- src/problem3.py
+++ src/problem3.py
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
+# DONE: 2.  READ the doc-string for the  sum_of_digits  and   is_prime
 # functions defined below.  They are the same as what you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
